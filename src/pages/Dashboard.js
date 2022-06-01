533db84ced1f03b92a76b433663db27fafd9cc87
Fahadjrdev <anonnyaishan@gmail.com>
2022-06-01 14:50:37 +0600

    some fixes

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -7,9 +7,9 @@ const Dashboard = () => {
       <Header pageTitle={`Dash board`} pageDesc ={`General indicators`} displaySearch={`show`} />
       <div className='main dashboard'>
         <div className="cards grid-container">   
-          <InvestorCard color={`primary-color`}  title={`Investors`} text={`271`} miniText={`+ 0,7% `} miniDisplay={`show`}/>
-          <InvestorCard color={`secondary-color`}  title={`Projects`} text={`69`} miniText={`- 1,7% `} miniDisplay={`show`} rotateArrowIcon={`down`}/>
-          <InvestorCard color={`tartiary-color`}  title={`New investors`} text={`5`} miniText={`+ 1% `} miniDisplay={`show`}/>
+          <InvestorCard key={1} color={`primary-color`}  title={`Investors`} text={`271`} miniText={`+ 0,7% `} miniDisplay={`show`}/>
+          <InvestorCard key={2} color={`secondary-color`}  title={`Projects`} text={`69`} miniText={`- 1,7% `} miniDisplay={`show`} rotateArrowIcon={`down`}/>
+          <InvestorCard key={3} color={`tartiary-color`}  title={`New investors`} text={`5`} miniText={`+ 1% `} miniDisplay={`show`}/>
         </div>
       </div>
     </>
--- src/pages/Investors.js
+++ src/pages/Investors.js
@@ -9,9 +9,9 @@ const Investors = () => {
       <Header pageTitle={`Investors`} pageDesc={`Investor Information`} displayBtn={`show`} textBtn={`Add investor`} colorBtn={`var(--primary-color)`} displaySearch={`show`} />
       <div className='main investor'>
         <div className="cards grid-container">   
-          <InvestorCard color={`primary-color`}  title={[`NET Amounts`, <br/> ,`per Investor`]} text={`105,551,274.20`} />
-          <InvestorCard color={`secondary-color`}  title={[`% of each`, <br/> ,`LP Commitment`]} text={`84.459%`} />
-          <InvestorCard color={`tartiary-color`}  title={`% of Total Called Capital`} text={`100.00%`} />
+          <InvestorCard key={1} color={`primary-color`}  title={[`NET Amounts`, <br key={1}/> ,`per Investor`]} text={`105,551,274.20`} />
+          <InvestorCard key={2} color={`secondary-color`}  title={[`% of each`, <br key={2}/> ,`LP Commitment`]} text={`84.459%`} />
+          <InvestorCard key={3} color={`tartiary-color`}  title={`% of Total Called Capital`} text={`100.00%`} />
         </div>
       </div>
     </>
